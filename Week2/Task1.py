ecfb5fcd7ea63a2503c8497282e3675a85602c40
Shahzeb21 <shahzebdawood@yahoo.com>
2016-11-29 14:26:50 +0000

    Add files via upload

---
+++ Week2/Task1.py
@@ -0,0 +1,14 @@
+def FindPerfSquare(Int):
+    
+    StartPos = int(round(Int/2)) #Halves the input number to decrease number of comparisons
+    
+    for i in range (2 , StartPos): #Since 0 or 1 wouldnt produce any perfect squares
+        Square = i*i
+        if (Square < Int+1):
+            MyNum = Square
+        else:
+            break
+    print(MyNum)
+    
+#FindPerfSquare(Check)               
+
---
+++ Week2/Task3.py
@@ -0,0 +1,43 @@
+def MatrixCalc(B,C,N,Operation):
+    
+    Result = []
+    for i in range(0 , N):
+        Result.append([])
+        for s in range(0 , N):
+            Result[i].append(0)
+            
+    if Operation == "+":
+    #For Addition:
+        for row in range (0 , N):
+            for col in range (0 , N):
+                (Result[row])[col] = (B[row])[col] + (C[row])[col]
+        print(Result)
+        
+    elif Operation == "-":
+    #For Subtraction:
+        for row in range (0 , N):
+            for col in range (0 , N):
+                (Result[row])[col] = (B[row])[col] - (C[row])[col]
+        print(Result)
+        
+    elif Operation == "*":
+    #For Multiplication:
+        
+        for row in range (0 , N):
+            for col in range (0 , N):
+                Sum = 0
+                for k in range (0 , N):
+                    Sum += B[row][k] * C[k][col]
+                    Result[row][col] = Sum
+        print(Result)
+
+r1 = MatrixCalc([(1,2),(3,4)],[(2,4),(6,8)], 2, "*")
+r2 = MatrixCalc([(1,2),(3,4)],[(2,4),(6,8)], 2, "+")
+r3 = 2*r2
+R = r1 + r3
+
+
+        
+
+#MatrixCalc([(1,2),(3,4)],[(1,2),(3,4)],2,"*")
+                   

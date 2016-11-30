7f9c6554c403dd32e70b5d0abc5c3cc57b8433a5
Shahzeb21 <shahzebdawood@yahoo.com>
2016-11-30 13:30:51 +0000

    FindPerfSquareCode

--- Week2/Task1.py
+++ Week2/Task1.py
@@ -1,6 +1,6 @@
 def FindPerfSquare(Int):
     
-    StartPos = int(round(Int/2)) #Halves the input number to decrease number of comparisons
+    StartPos = int(round(Int/2)) #Halves the input number in order to decrease number of comparisons
     
     for i in range (2 , StartPos): #Since 0 or 1 wouldnt produce any perfect squares
         Square = i*i

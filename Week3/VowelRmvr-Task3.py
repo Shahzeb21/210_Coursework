3956241fc45fb4bf60b1f53dc2e826561a0425b5
Shahzeb21 <shahzebdawood@yahoo.com>
2016-12-02 13:11:31 +0000

    Update VowelRmvr-Task3.py

--- Week3/VowelRmvr-Task3.py
+++ Week3/VowelRmvr-Task3.py
@@ -7,9 +7,9 @@ def VowelRmvr(word):
     
     else:
         if word[0] in Vowels:
-            return VowelRmvr(word[1:])
+            return VowelRmvr(word[1:]) #If first letter of string is a vowel, then calls the function again withouth the first letter
         else:
-            return word[0] + VowelRmvr(word[1:])
+            return word[0] + VowelRmvr(word[1:])# if first letter isnt a vowel, the non-vowel letter is returned and the function recalled.
 
 print(VowelRmvr("beautiful"))
 

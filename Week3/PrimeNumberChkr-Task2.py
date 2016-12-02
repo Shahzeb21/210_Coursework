b19e892f9992856828485fca68d185d0d84e8fec
Shahzeb21 <shahzebdawood@yahoo.com>
2016-12-02 13:09:34 +0000

    Update PrimeNumberChkr-Task2.py

--- Week3/PrimeNumberChkr-Task2.py
+++ Week3/PrimeNumberChkr-Task2.py
@@ -1,14 +1,20 @@
 def prime(n,m):
+    # where n is the number we want to check is prime
+    # and m is equal to n-1
 
     if n < 2: #Base Case
         
         print("not a prime number")
         return False
+    
+    if n == 2:
+        print("prime number")
+        return True
 
     else: #Terminating Cases
-     
+        
         if n % m == 0:
-            print("not a prime number")
+            print("not a prime number") #this covers the case of 2 and 1. 
             return False
         
         elif n % m != 0 and m != 2:

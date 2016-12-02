d7c94a22a8cdb01e36648ec59d032b869ad29715
Shahzeb21 <shahzebdawood@yahoo.com>
2016-12-02 13:13:36 +0000

    Update StringReverse-Task1.py

--- Week3/StringReverse-Task1.py
+++ Week3/StringReverse-Task1.py
@@ -1,12 +1,12 @@
 def StringReverse(Sentence):
     
     NewList = []
-    s = (Sentence.split(" "))   
+    s = (Sentence.split(" ")) # s is a list of all the words in the sentence
     
     for i in range (len(s) , 0 , -1):
-        NewList.append(s[i-1])
+        NewList.append(s[i-1]) #simply adds all the words in s form the bottom up to NewList
          
-    Final = ' '.join(NewList)
+    Final = ' '.join(NewList) #joins the elements of the list to make them a sentence
     print(Final)
   
 StringReverse("This is awesome i cant believe what is happening right now!")

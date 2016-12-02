782ad7c3ad357fa17a2c7042123b7fe231733260
Shahzeb21 <shahzebdawood@yahoo.com>
2016-12-02 12:55:29 +0000

    Update Task1.py

--- Week2/Task1.py
+++ Week2/Task1.py
@@ -1,10 +1,10 @@
-def FindPerfSquare(Int):
+def FindPerfSquare(Number):
     
-    StartPos = int(round(Int/2)) #Halves the input number in order to decrease number of comparisons
+    StartPos = int(round(Number/2)) #Halves the input number in order to decrease number of comparisons
     
     for i in range (2 , StartPos): #Since 0 or 1 wouldnt produce any perfect squares
         Square = i*i
-        if (Square < Int+1):
+        if (Square < Number+1):
             MyNum = Square
         else:
             break
